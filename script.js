9842796ec6bf0b3693e35b2e1436ee063d3d71fd
ViniciusGasiunas <viniciusgasiunas@outlook.com>
2019-07-28 17:38:48 -0300

    Template base para projetos

---
+++ script.js
@@ -0,0 +1,18 @@
+// let lista = document.createElement('li');
+// let ul = document.querySelector('.menu ul');
+// let logado = true;
+
+// if(logado){
+//     let texto = document.createTextNode ('Olá, mundo!');
+// lista.appendChild(texto);
+// ul.appendChild(lista);    
+// }else{
+//     let texto = document.createTextNode("Login");
+//     lista.appendChild(texto);
+//     ul.appendChild(lista);
+// }
+
+function abrir() {
+    let menu = document.querySelectorAll('.menu .nav')
+    
+}
